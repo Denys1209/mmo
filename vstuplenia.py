4d92dd46cd747e9bcbc8c47ca0b52583ecd03fad
Denys <63914276+Denys1209@users.noreply.github.com>
2020-10-25 19:18:19 +0200

    Delete vstuplenia.py

--- vstuplenia.py
+++
@@ -1,9 +0,0 @@
-import pygame
-from setting import sc
-def loga():
-    loga = pygame.image.load("D:\\Denis\\loga.png")
-    compani = pygame.image.load("D:\\Denis\\compani.png")
-    sc.blit(loga, (0, 0))
-    sc.blit(compani, (0, 300))
-    pygame.display.update()
-    
